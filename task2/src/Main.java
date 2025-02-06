a6693eab1455e0884434cda74d2610f9229289ec
flawlessmevin <vanyaalisov10@gmail.com>
2025-02-06 21:08:09 +0100

    gitignore fix

---
+++ task2/src/Main.java
@@ -0,0 +1,98 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<List<Integer>> reports = new ArrayList<>();
+        int safe_reports_count = 0; //for part 1
+        int safe_reports_count2 = 0; //for part 2
+
+        List<List<Integer>> safe_reports = new ArrayList<>();
+        List<List<Integer>> unsafe_reports = new ArrayList<>();
+
+        //read input file and split each report to list of levels and put each to reports list
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+            String line;
+            while ((line = br.readLine()) != null ) {
+                String[] parts = line.split("\\s+");
+
+                List<Integer> report = new ArrayList<>();
+
+                for (String part : parts) {
+                    report.add(Integer.parseInt(part));
+                }
+                reports.add(report);
+            }
+        } catch (IOException e){
+            System.out.println(e);
+            }
+
+
+        //part 1
+        for (List<Integer> report : reports) {
+
+            boolean safe = check_safety(report);
+
+            if (safe) {
+                safe_reports_count++;
+                safe_reports.add(report);
+            } else {
+                unsafe_reports.add(report);
+            }
+        }
+        //answer part 1
+        System.out.println("answer 1 part: " + safe_reports_count);
+
+
+
+
+        //part 2
+        for (List<Integer> report : unsafe_reports) {
+            for (int i = 0; i < report.size(); i++) {
+                List<Integer> report_copy = new ArrayList<>(report);
+                report_copy.remove(i);
+                boolean safe = check_safety(report_copy);
+                if (safe) {
+                    safe_reports_count2++;
+                    break;
+                }
+            }
+        }
+        //answer part 2
+        int all_safe_reports_count = safe_reports_count + safe_reports_count2;
+        System.out.println("answer 2 part: " + all_safe_reports_count);
+
+    }
+
+    //check safety function
+    public static boolean check_safety(List<Integer>report) {
+        int levels_count = report.size();
+        boolean up = false;
+        boolean safe = true;
+
+        if (report.get(0) < report.get(1)) {
+            up = true;
+        }
+
+        for (int i = 1; i < levels_count; i++) {
+            int current_level = report.get(i);
+            int previous_level = report.get(i - 1);
+
+            if (up){
+                int increase = current_level - previous_level;
+                if (increase != 1 & increase !=2 & increase !=3){
+                    safe = false;
+                }
+            } else{
+                int decrease = current_level - previous_level;
+                if (decrease != -1 & decrease != -2 & decrease != -3){
+                    safe = false;
+                }
+            }
+        }
+        return safe;
+    }
+}
