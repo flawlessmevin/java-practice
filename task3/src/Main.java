c18ad8e3c0ecc1ba679decb6cb50d23de908a74e
flawlessmevin <161849058+flawlessmevin@users.noreply.github.com>
2025-02-22 16:27:27 +0100

    task 3 - part 2 optimisation

--- task3/src/Main.java
+++ task3/src/Main.java
@@ -3,7 +3,6 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
-        String full_input = "";
         int sum = 0;
         String mul_example = "mul\\((\\d+),(\\d+)\\)";
 
@@ -12,36 +11,34 @@ public class Main {
         String dont_do_example = "don't\\(\\)";
         boolean enable = true;
 
-        //read file and put in String
+
+        Pattern pattern = Pattern.compile(mul_example + "|" + do_example + "|" + dont_do_example);
+
         try {
             BufferedReader br = new BufferedReader(new FileReader("input.txt"));
             String line;
             while ((line = br.readLine()) != null) {
-                full_input += line;
+                Matcher matcher = pattern.matcher(line);
+                while (matcher.find()) {
+                    if (matcher.group().equals("do()")){
+                        enable = true;
+                    } else if(matcher.group().equals("don't()")){
+                        enable = false;
+                    } else if (enable){
+                        int a = Integer.parseInt(matcher.group(1));
+                        int b = Integer.parseInt(matcher.group(2));
+                        sum += a * b;
+                    }
+                }
             }
             br.close();
         } catch (IOException e) {
             System.out.println(e);
         }
 
-
-        Pattern pattern = Pattern.compile(mul_example + "|" + do_example + "|" + dont_do_example);
-        Matcher matcher = pattern.matcher(full_input);
-        while (matcher.find()) {
-            if (matcher.group().equals("do()")){
-                enable = true;
-            } else if(matcher.group().equals("don't()")){
-                enable = false;
-            } else if (enable){
-                int a = Integer.parseInt(matcher.group(1));
-                int b = Integer.parseInt(matcher.group(2));
-                sum += a * b;
-            }
-        }
-
         //answer
         System.out.println(sum);
 
 
-        }
     }
+}
