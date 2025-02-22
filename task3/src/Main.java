d8db3cecea27d8ec467398d3ce9ea0893547abd8
flawlessmevin <161849058+flawlessmevin@users.noreply.github.com>
2025-02-22 16:16:21 +0100

    task 3 - part 2

--- task3/src/Main.java
+++ task3/src/Main.java
@@ -3,41 +3,45 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
+        String full_input = "";
         int sum = 0;
-        String example = "mul\\((\\d+),(\\d+)\\)";
+        String mul_example = "mul\\((\\d+),(\\d+)\\)";
 
         //for part 2
         String do_example = "do\\(\\)";
-        String dont_do_example = "dont\\(\\)";
-        Boolean enable = true;
+        String dont_do_example = "don't\\(\\)";
+        boolean enable = true;
 
+        //read file and put in String
         try {
             BufferedReader br = new BufferedReader(new FileReader("input.txt"));
             String line;
-
-            Pattern mul_pattern = Pattern.compile(example);
-
             while ((line = br.readLine()) != null) {
-                
-                Matcher matcher = mul_pattern.matcher(line);
-
-                while (matcher.find()) {
-                    int a = Integer.parseInt(matcher.group(1));
-                    int b = Integer.parseInt(matcher.group(2));
-
-                    sum += a * b;
-                }
+                full_input += line;
             }
             br.close();
-
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println(e);
         }
 
 
+        Pattern pattern = Pattern.compile(mul_example + "|" + do_example + "|" + dont_do_example);
+        Matcher matcher = pattern.matcher(full_input);
+        while (matcher.find()) {
+            if (matcher.group().equals("do()")){
+                enable = true;
+            } else if(matcher.group().equals("don't()")){
+                enable = false;
+            } else if (enable){
+                int a = Integer.parseInt(matcher.group(1));
+                int b = Integer.parseInt(matcher.group(2));
+                sum += a * b;
+            }
+        }
+
+        //answer
         System.out.println(sum);
 
 
-
+        }
     }
-}
