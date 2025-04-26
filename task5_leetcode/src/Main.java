d23bcaad07f32b6f9f1097e4beda54286dc7a8a7
flawlessmevin <161849058+flawlessmevin@users.noreply.github.com>
2025-04-26 17:36:25 +0200

    task5_leetcode - optimiszation
    use HashMap for O(n)

--- task5_leetcode/src/Main.java
+++ task5_leetcode/src/Main.java
@@ -1,26 +1,29 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
         int[] nums = {2,7,11,15};
+
         int target = 9;
 
         int[] result = result(nums, target);
 
         System.out.println(Arrays.toString(result));
-
     }
 
     public static int[] result(int[] numbers, int target){
+        HashMap<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i+1; j < numbers.length; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    return new int[]{i+1, j+1};
-                }
+            int difference = target - numbers[i];
+            if(map.containsKey(difference)){
+                return new int[]{map.get(difference) + 1,i + 1};
             }
+            map.put(numbers[i],i);
         }
         return new int[]{0,0};
     }
+
 
 
 }
